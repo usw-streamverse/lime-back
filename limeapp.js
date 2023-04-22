a88ddc1330f8e89b47f10a3c32a7ffcbd2f1cca6
kimdw524 <kimdw524@naver.com>
2023-04-22 23:30:35 +0900

    refactor: users->auth, 환경변수 사용

--- limeapp.js
+++ limeapp.js
@@ -1,9 +1,10 @@
 const express = require('express'); //익스프레스 모듈 삽입
 const app = express();
-const usersRouter = require('./routes/users');
+require('dotenv').config();
+const authRouter = require('./routes/auth');
 app.set('port',process.env.PORT || 3000); //process.env에 포트속성이 있다면 사용, 아니라면 3000.
 
-app.use('/users', usersRouter);
+app.use('/auth', authRouter);
 
 app.listen(app.get('port'), () => {
    console.log(`lime-backend is running on port ${app.get('port')}`);
