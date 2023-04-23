aebe53dd1df2041d3f3921080345b8e856b96a26
kimdw524 <kimdw524@naver.com>
2023-04-23 22:34:40 +0900

    feat: add body-parser

--- limeapp.js
+++ limeapp.js
@@ -1,10 +1,12 @@
 const express = require('express'); //익스프레스 모듈 삽입
 const app = express();
+const bodyParser = require('body-parser');
 const authRouter = require('./routes/auth');
 
 require('dotenv').config();
 
 app.use(require('cors')());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set('port',process.env.PORT || 3000); //process.env에 포트속성이 있다면 사용, 아니라면 3000.
 
