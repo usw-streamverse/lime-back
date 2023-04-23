37c9752d81f2abd3d885fcd45bd52f41d0c58dd2
kimdw524 <kimdw524@naver.com>
2023-04-23 22:50:40 +0900

    feat: add bodyParser.json()

--- limeapp.js
+++ limeapp.js
@@ -6,7 +6,9 @@ const authRouter = require('./routes/auth');
 require('dotenv').config();
 
 app.use(require('cors')());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
 
 app.set('port',process.env.PORT || 3000); //process.env에 포트속성이 있다면 사용, 아니라면 3000.
 
