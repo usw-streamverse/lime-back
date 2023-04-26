6e42b6df855609e14d4746b7ecd0a65355baf093
kimdw524 <kimdw524@naver.com>
2023-04-27 00:16:30 +0900

    comment: auth middleware 설명 주석 추가

--- limeapp.js
+++ limeapp.js
@@ -9,6 +9,12 @@ app.use(require('cors')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+/*
+로그인을 해야만 접근 가능한 라우터에는
+   app.use(auth).use('/exmaple', exampleRouter);
+이런 식으로 auth 미들웨어 사용하면 됨
+라우터 안에서 id를 전달받으려면 req.id에 로그인 한 id가 들어있음
+*/
 
 app.set('port',process.env.PORT || 3000);
 
