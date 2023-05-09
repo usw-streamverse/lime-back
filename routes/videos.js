48c69c3d409dd8a12e4640d0be29b3aad9d5c329
kimdw524 <kimdw524@naver.com>
2023-05-09 22:35:17 +0900

    feat: video router

---
+++ routes/videos.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const db = require('../db/db.js');
+const fs = require('fs');
+const multer = require('multer')
+const auth = require('../middlewares/auth');
+const router = express.Router();
+const MulterAzureStorage = require('multer-azure-blob-storage').MulterAzureStorage;
+
+const connection = process.env.STORAGE_CONNECTION_STRING;
+const accessKey = process.env.AZURE_STORAGE_ACCOUNT_ACCESS_KEY;
+const accountName = process.env.AZURE_STORAGE_ACCOUNT_NAME;
+const containerName = 'lime';
+
+const azureStorage = new MulterAzureStorage({
+    connectionString: connection,
+    accessKey: accessKey,
+    accountName: accountName,
+    containerName: containerName,
+    urlExpirationTime: 60
+});
+    
+const upload = multer({ storage : azureStorage});
+
+router.post('/', auth, upload.any('video'), (req, res) => {
+    if(req.files){
+        res.status(200).json({
+            success: true,
+            files: req.files
+        });
+    } else {
+        res.status(404).json(
+            {success: false}
+        )
+    }
+});
+
+module.exports = router;
