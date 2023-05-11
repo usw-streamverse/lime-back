6c368f4468cebc5440290262943f7e1126ed1dd5
kimdw524 <kimdw524@naver.com>
2023-05-11 21:49:03 +0900

    feat: thumbnail

--- routes/videos.js
+++ routes/videos.js
@@ -32,7 +32,7 @@ const storage = multer.diskStorage({
         cb(null, 'storage')
     },
     filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
+        cb(null, 'v' + Date.now())
     }
 });
 
@@ -70,68 +70,80 @@ router.post('/', auth, (req, res) => {
             }
         }
         const tempFolder = `storage/${req.file.filename}_`;
-        let duration = 0;
+        let duration = 0, thumbnail = '';
 
         if(!fs.existsSync(tempFolder))
             fs.mkdirSync(tempFolder, { recursive: true });
         ffmpeg(req.file.path)
-            .outputOption([
-                '-codec: copy',
-                '-hls_time 10',
-                '-hls_list_size 0',
-                '-hls_playlist_type vod',
-                '-hls_flags split_by_time',
-                '-hls_base_url https://svlimestorage.blob.core.windows.net/lime/',
-                `-hls_segment_filename ${tempFolder}/${req.file.filename}-%d.ts`
-            ])
-            .output(`${tempFolder}/result.m3u8`)
-            .on('error', () => {
-                if(fs.existsSync(`storage/${req.file.filename}`))
-                    fs.unlinkSync(`storage/${req.file.filename}`);
-                if(fs.existsSync(tempFolder))
-                    fs.rmSync(tempFolder, { recursive: true, force: true });
-                res.status(415).json({
-                    success: false
+            .screenshots({
+                timestamps: ['50%'],
+                filename: `thumbnail.png`,
+                folder: tempFolder,
+                size: '320x180'
+            })
+            .on('end', () => {
+                ffmpeg(req.file.path)
+                .outputOption([
+                    '-codec: copy',
+                    '-hls_time 10',
+                    '-hls_list_size 0',
+                    '-hls_playlist_type vod',
+                    '-hls_flags split_by_time',
+                    '-hls_base_url https://svlimestorage.blob.core.windows.net/lime/',
+                    `-hls_segment_filename ${tempFolder}/${req.file.filename}-%d.ts`
+                ])
+                .output(`${tempFolder}/result.m3u8`)
+                .on('error', () => {
+                    if(fs.existsSync(`storage/${req.file.filename}`))
+                        fs.unlinkSync(`storage/${req.file.filename}`);
+                    if(fs.existsSync(tempFolder))
+                        fs.rmSync(tempFolder, { recursive: true, force: true });
+                    res.status(415).json({
+                        success: false
+                    })
                 })
-            })
-            .on('codecData', (data) => {
-                duration = toSec(data.duration);
-            })
-            .on('end', async () => {
-                console.log('end');
-                if(fs.existsSync(`storage/${req.file.filename}`))
-                    fs.unlinkSync(`storage/${req.file.filename}`);
+                .on('codecData', (data) => {
+                    duration = toSec(data.duration);
+                })
+                .on('end', async () => {
+                    console.log('end');
+                    if(fs.existsSync(`storage/${req.file.filename}`))
+                        fs.unlinkSync(`storage/${req.file.filename}`);
+    
+                    let urls = {m3u8: '', segments: []};
+                    
+                    let blockBlobClient = containerClient.getBlockBlobClient(`${req.file.filename}.m3u8`);
+                    let uploadBlobResponse = await blockBlobClient.uploadFile(`${tempFolder}/result.m3u8`);
+                    urls.m3u8 = blockBlobClient.url;
 
-                let urls = {m3u8: '', segments: []};
-                
-                let blockBlobClient = containerClient.getBlockBlobClient(`${req.file.filename}.m3u8`);
-                let uploadBlobResponse = await blockBlobClient.uploadFile(`${tempFolder}/result.m3u8`);
-                
-                urls.m3u8 = blockBlobClient.url;
-
-                let fileList = fs.readdirSync(tempFolder);
-                for(const e of fileList){
-                    if(e.split('.')[1] === 'ts'){
-                        let blockBlobClient = containerClient.getBlockBlobClient(e);
-                        let uploadBlobResponse = await blockBlobClient.uploadFile(`${tempFolder}/${e}`);
-                        urls.segments.push(blockBlobClient.url);
-                    }
-                };
-                db.query('INSERT INTO video (url, own_channel, duration, title, explanation, image) VALUES (?,?,?,?,?,?);', [JSON.stringify(urls), req.id, duration, '테스트', '테스트입니다.', 'https://test/1.png'],
-                (error) => {
-                    if(error){
-                        res.status(500).json({
-                            success: false
-                        })  
-                    }else{
-                        res.status(200).json({
-                            m3u8: urls.m3u8,
-                            success: true
-                        })
-                    } 
-                });
+                    blockBlobClient = containerClient.getBlockBlobClient(`${req.file.filename}-thumbnail.png`);
+                    uploadBlobResponse = await blockBlobClient.uploadFile(`${tempFolder}/thumbnail.png`);
+                    thumbnail = blockBlobClient.url;
+    
+                    let fileList = fs.readdirSync(tempFolder);
+                    for(const e of fileList){
+                        if(e.split('.')[1] === 'ts'){
+                            let blockBlobClient = containerClient.getBlockBlobClient(e);
+                            let uploadBlobResponse = await blockBlobClient.uploadFile(`${tempFolder}/${e}`);
+                            urls.segments.push(blockBlobClient.url);
+                        }
+                    };
+                    db.query('INSERT INTO video (url, own_channel, duration, title, explanation, image) VALUES (?,?,?,?,?,?);', [JSON.stringify(urls), req.id, duration, '테스트', '테스트입니다.', thumbnail],
+                    (error) => {
+                        if(error){
+                            res.status(500).json({
+                                success: false
+                            })  
+                        }else{
+                            res.status(200).json({
+                                m3u8: urls.m3u8,
+                                success: true
+                            })
+                        } 
+                    });
+                })
+                .run();
             })
-            .run();
     });
 });
 
