45c02348772b116850cda037b9a103cc04495cc1
kimdw524 <kimdw524@naver.com>
2023-05-11 17:07:55 +0900

    feat: multer error handling

--- routes/videos.js
+++ routes/videos.js
@@ -16,10 +16,10 @@ const containerName = 'lime';
 if(!fs.existsSync('storage')) fs.mkdirSync('storage');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
+    destination: (req, file, cb) => {
         cb(null, 'storage')
     },
-    filename: function (req, file, cb) {
+    filename: (req, file, cb) => {
         cb(null, file.fieldname + '-' + Date.now())
     }
 });
@@ -33,19 +33,9 @@ const azureStorage = new MulterAzureStorage({
 });
     
 const upload = multer({
-    storage : azureStorage,
-    fileFilter: (req, file, callback) => {
-        let ext = path.extname(file.originalname);
-        let allowed = ['.mp4', '.webm', '.avi', '.ts'];
-        if(allowed.indexOf(ext) > -1)
-            return callback(null, true);
-        else
-            return callback(new Error('Only videos are allowed.'), false);
-    },
-    limits:{
-        fileSize: 1024 ** 3
-    }
+    storage : azureStorage
 });
+
 const localUpload = multer({
     storage: storage,
     fileFilter: (req, file, callback) => {
@@ -53,47 +43,65 @@ const localUpload = multer({
         let allowed = ['.mp4', '.webm', '.avi', '.ts'];
         if(allowed.indexOf(ext) > -1)
             return callback(null, true);
-        else
-            return callback(new Error('Only videos are allowed.'), false);
+        else {
+            const err = new Error('Only videos are allowed.');
+            err.statusCode = 406;
+            return callback(err, false);
+        }
     },
     limits:{
-        fileSize: 1024 ** 3
+        fileSize: 1024 ** 3 // 업로드 파일 최대 크기 1GB
     }
 });
 
-//router.post('/', auth, upload.any('video'), (req, res) => {
-router.post('/', auth, localUpload.any('video'), (req, res) => {
-    const tempFolder = `storage/${req.files[0].filename}_`;
-    if(!fs.existsSync(tempFolder))
-        fs.mkdirSync(tempFolder, {recursive: true});
-    ffmpeg(req.files[0].path)
-        .outputOption([
-            '-codec: copy',
-            '-hls_time 10',
-            '-hls_list_size 0',
-            '-hls_playlist_type vod',
-            '-hls_flags split_by_time',
-            '-hls_base_url http://localhost:3000/',
-            `-hls_segment_filename ${tempFolder}/%d.ts`
-        ])
-        .output(`${tempFolder}/result.m3u8`)
-        .on('progress', function(progress) {
-            console.log('Processing: ' + progress.percent + '% done')
-        })
-        .on('end', function(err, stdout, stderr) {
-            console.log('Finished processing!' /*, err, stdout, stderr*/)
-        })
-        .run();
-    if(req.files){
-        res.status(200).json({
-            success: true,
-            ...req.files[0]
-        });
-    } else {
-        res.status(404).json(
-            {success: false}
-        )
-    }
+router.post('/', auth, (req, res) => {
+    localUpload.single('video')(req, res, (err) => {
+        if(err || !req.file){
+            if(err.statusCode){
+                res.status(err.statusCode).json({
+                    success: false
+                })
+                return;
+            } else {
+                res.status(500).json({
+                    success: false
+                })
+                return;
+            }
+        }
+        
+        const tempFolder = `storage/${req.file.filename}_`;
+        if(!fs.existsSync(tempFolder))
+            fs.mkdirSync(tempFolder, { recursive: true });
+        ffmpeg(req.file.path)
+            .outputOption([
+                '-codec: copy',
+                '-hls_time 10',
+                '-hls_list_size 0',
+                '-hls_playlist_type vod',
+                '-hls_flags split_by_time',
+                '-hls_base_url http://localhost:3000/',
+                `-hls_segment_filename ${tempFolder}/%d.ts`
+            ])
+            .output(`${tempFolder}/result.m3u8`)
+            .on('error', () => {
+                if(fs.existsSync(`storage/${req.file.filename}`))
+                    fs.unlinkSync(`storage/${req.file.filename}`);
+                if(fs.existsSync(`storage/${req.file.filename}_`))
+                    fs.rmSync(`storage/${req.file.filename}_`, { recursive: true, force: true });
+                res.status(415).json({
+                    success: false
+                })
+            })
+            .on('end', () => {
+                if(fs.existsSync(`storage/${req.file.filename}`))
+                    fs.unlinkSync(`storage/${req.file.filename}`);
+                res.status(200).json({
+                    success: true
+                })
+            })
+            .run();
+    });
 });
 
 module.exports = router;
