f7e37d8c098bb2264b6fe3581bb735396d3d6706
kimdw524 <kimdw524@naver.com>
2023-05-17 12:43:25 +0900

    feat: mysql pool 사용
    장시간 방치 시 mysql 연결 끊기는 문제 해결

--- db/db.js
+++ db/db.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const fs = require('fs');
 
 const config = {
+    connectionLimit: 10,
     host: 'svlime-database.mysql.database.azure.com',
     user: 'lime_admin',
     password: 'Suwon18017',
@@ -10,18 +11,6 @@ const config = {
     ssl: {ca: fs.readFileSync(`${__dirname}/DigiCertGlobalRootCA.crt.pem`)}
 }
 
-const conn = new mysql.createConnection(config);
-
-conn.connect(
-    function (err) { 
-    if (err) { 
-        console.log("!!! Cannot connect !!! Error:");
-        throw err;
-    }
-    else
-    {
-        console.log("Connection established.");
-    }
-});
+const conn = mysql.createPool(config);
 
 module.exports = conn;
