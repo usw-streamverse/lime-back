f11dde0783ec7fac8818728524ed20b93b1e6616
kimdw524 <kimdw524@naver.com>
2023-05-18 23:50:37 +0900

    feat: 비로그인이라도 접근 허용하고 로그인 여부만 파악하는 로직 추가

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,28 @@
 const jwt = require('jsonwebtoken');
 const jwt_config = require('../config/jwt.js');
 
-const auth = (req, res, next) => {
-    let token = req.get('Authorization');
-    if(token == null)
-        res.status(403).json({ message: 'permission denied.' });
-    else {
-        token = token.split('Bearer ');
-        if(token.length > 1) token = token[1];
-        else token = token[0];
-        jwt.verify(token, jwt_config.secretKey, (error, decoded) => {
-            if(error)
-                res.status(403).json({ message: 'invalid token.' });
-            else{
-                req.id = decoded.id;
-                req.userid = decoded.userid;
+const auth = (strict = true) => {
+    return (req, res, next) => {
+        let token = req.get('Authorization');
+        if(token == null){
+            if(strict)
+                res.status(403).json({ message: 'permission denied.' });
+            else
                 next();
-            }
-        })
+        } else {
+            token = token.split('Bearer ');
+            if(token.length > 1) token = token[1];
+            else token = token[0];
+            jwt.verify(token, jwt_config.secretKey, (error, decoded) => {
+                if(error) {
+                    if(strict) res.status(403).json({ message: 'invalid token.' });
+                } else{
+                    req.id = decoded.id;
+                    req.userid = decoded.userid;
+                    next();
+                }
+            })
+        }
     }
 };
 
