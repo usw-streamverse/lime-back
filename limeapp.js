f4d15f11bcfc4eecd136de3b36172f689164b4ae
I4CU <129958775+IforCU@users.noreply.github.com>
2023-08-23 21:47:38 +0900

    Update limeapp.js 구독 목록 기능

--- limeapp.js
+++ limeapp.js
@@ -17,6 +17,7 @@ app.use('/auth', require('./routes/auth')); //로그인페이지
 app.use('/videos', require('./routes/videos')); //비디오 업로드
 app.use('/channels', require('./routes/channels')); //채널
 app.use('/search', require('./routes/search')); //검색
+app.use('/subscribe', require('./routes/subscribe')); // 구독
 
 app.use((req, res, nest) => { //찾을 수 없다면.
    res.status(404).send('Not Found');
