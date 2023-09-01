240608ea560f4343e11a787d149d41eae646e172
I4CU <129958775+IforCU@users.noreply.github.com>
2023-09-01 20:59:58 +0900

    Update subscribe.js 오류 수정

--- routes/subscribe.js
+++ routes/subscribe.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 
 router.get('/',auth() ,(req, res) => {
-    db.query('SELECT video.id, user.nickname, video.created, video.duration, video.title, video.view_count, video.thumbnail FROM video LEFT JOIN user ON video.channel_id = user.id LEFT JOIN subscribe ON video.channel_id = subscribe.channel WHERE subscribe.subscriber = ? and status = \'ACTIVE\' ORDER BY created DESC', [req.id], 
+    db.query('SELECT video.id, user.nickname, video.created, video.duration, video.title, video.view_count, video.thumbnail FROM video LEFT JOIN user ON video.channel_id = user.id LEFT JOIN subscribe ON video.channel_id = subscribe.channel WHERE subscribe.subscriber = ? and video.status = \'ACTIVE\' ORDER BY created DESC', [req.id], 
     (error, result) => {
         if(error) throw error;
         res.status(200).json(result);
