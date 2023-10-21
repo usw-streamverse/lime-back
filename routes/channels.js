0918ee7306a8b4681f2318daa4bd1defca0b99bd
kimdw524 <kimdw524@naver.com>
2023-10-21 19:50:05 +0900

    feat: 재생 목록 조회 api

--- routes/channels.js
+++ routes/channels.js
@@ -75,16 +75,17 @@ router.post('/playlist', auth(), (req, res) => {  // 재생목록을 만듬
     }
 });
 
-router.get('/playlist', auth(), (req, res) => {  // 재생목록을 확인하는 기능
-    db.query('SELECT playlist.id, playlist.name, playlist.created, COUNT(playlist_item.playlist_id) AS count, (SELECT video.thumbnail FROM video LEFT JOIN playlist_item ON playlist_item.video_id = video.id WHERE playlist_item.playlist_id = playlist.id ORDER BY playlist_item.created DESC LIMIT 1) AS thumbnail FROM playlist LEFT JOIN playlist_item ON playlist.id = playlist_item.playlist_id WHERE user_id = ? GROUP BY playlist.id ORDER BY playlist.created DESC', [req.id], 
-        async (error, result) => {
+// 특정 채널의 재생 목록 조회
+router.get('/:id/playlist', auth(false), (req, res) => {
+    db.query('SELECT playlist.id, playlist.name, playlist.created, COUNT(playlist_item.playlist_id) AS count, (SELECT video.thumbnail FROM video LEFT JOIN playlist_item ON playlist_item.video_id = video.id WHERE playlist_item.playlist_id = playlist.id ORDER BY playlist_item.created DESC LIMIT 1) AS thumbnail FROM playlist LEFT JOIN playlist_item ON playlist.id = playlist_item.playlist_id WHERE user_id = ? GROUP BY playlist.id ORDER BY playlist.created DESC', [req.params.id == 0 ? req.id : req.params.id], 
+        (error, result) => {
             if(error) throw error;
             res.status(200).json(result);
         }
     );
 });
 
-//특정 재생 목록에 들어가 있는 동영상 목록 출력
+// 특정 재생 목록에 들어가 있는 동영상 목록 출력
 router.get('/playlist/:id', (req, res) => {
     db.query('SELECT playlist_item.video_id, playlist_item.created, video.title, video.thumbnail, video.duration FROM playlist_item LEFT JOIN video on video.id = playlist_item.video_id WHERE playlist_id = ? ORDER BY created DESC', [req.params.id], 
         async (error, result) => {
@@ -94,7 +95,7 @@ router.get('/playlist/:id', (req, res) => {
     );
 });
 
-//재생 목록 삭제
+// 재생 목록 삭제
 router.delete('/playlist/:id', auth(), (req, res) => {
     db.query('DELETE FROM playlist WHERE id = ? and user_id = ?', [req.params.id, req.id], 
         async (error, result) => {
@@ -109,5 +110,24 @@ router.delete('/playlist/:id', auth(), (req, res) => {
     );
 });
 
+// 재생 목록에 있는 동영상 삭제
+router.delete('/playlist/:playlist_id/:video_id', auth(), (req, res) => {
+    db.query('SELECT user_id FROM playlist WHERE id = ?', [req.params.playlist_id], 
+        (error, result) => {
+            if (error) throw error;
+            if (result.length > 0 && result[0].user_id == req.id) {
+                db.query('DELETE FROM playlist_item WHERE playlist_id = ? and video_id = ?', [req.params.playlist_id, req.params.video_id], 
+                    (error, result) => {
+                        if (error) throw error;
+                        res.status(200).json({success: true});
+                    }
+                );
+            } else {
+                res.status(401).json({success: false});
+            }
+        }
+    );
+    
+});
 
 module.exports = router;
