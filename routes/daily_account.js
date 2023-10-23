9fe06211984d319601713282905a2f68c8931cb4
kimdw524 <kimdw524@naver.com>
2023-10-23 21:10:50 +0900

    Update daily_account.js

--- routes/daily_account.js
+++ routes/daily_account.js
@@ -3,23 +3,19 @@ const db = require('../db/db.js');
 const express = require('express');
 const router = express.Router();
 
-var daily_best;
+let daily_best = [];
 cron.schedule('* * * * *', () => {  // 테스트용으로 1분마다 실행. 실전 : 매일 자정 05분에 실행 = '5 0 * * *' 
     console.log('node-cron 실행 테스트, 1분마다 실행', new Date().toString());
     //전날 시청 동영상통계. 시청 수 상위 5개의 동영상만 선정.
-    db.query("select A.id, title, total_view, duration, thumbnail, user.nickname from ((select id, sum(frequency) as total_view from recent_popular_video where date_format(update_time,'%Y-%m-%d') = (CURDATE()) group by id order by total_view desc limit 5) A inner join video on a.id = video.id ) inner join user on user.id = video.channel_id;", // recent_popular_video에서 현재의 전날 날짜에 해당하는 데이터만 가공.
-    (error,result) => {
-      if(error) throw error;
-      else {
-        daily_best = result; 
-      }
+    db.query("select A.id, title, total_view, duration, thumbnail, user.nickname, user.profile, video.view_count, video.created from ((select id, sum(frequency) as total_view from recent_popular_video where date_format(update_time,'%Y-%m-%d') = (CURDATE()) group by id order by total_view desc limit 5) A inner join video on a.id = video.id ) inner join user on user.id = video.channel_id;", // recent_popular_video에서 현재의 전날 날짜에 해당하는 데이터만 가공.
+    (error, result) => {
+      if (error) throw error;
+      daily_best = result;
     });
   });
 
 router.get('/', (req, res) => {
-    res.status(200).json({
-        "best": daily_best
-    })
+    res.status(200).send(daily_best)
 });
 
 module.exports = router;
