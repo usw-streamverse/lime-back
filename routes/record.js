d16fe7535706d7dcf13b48d313f0708927183bd6
kimdw524 <kimdw524@naver.com>
2023-10-23 21:31:55 +0900

    Update record.js

--- routes/record.js
+++ routes/record.js
@@ -4,18 +4,10 @@ const router = express.Router();
 const auth = require('../middlewares/auth');
 
 router.get('/',auth() ,(req, res) => {
-    db.query('SELECT video.id, user.nickname, video.created, video.duration, video.title, video.view_count, video.thumbnail FROM video LEFT JOIN user ON video.channel_id = user.id LEFT JOIN record ON video.id = record.video_id WHERE record.user_id = ?  ORDER BY record.updated DESC;', [req.id], 
+    db.query('SELECT video.id, user.nickname, user.profile, video.created, video.duration, video.title, video.view_count, video.thumbnail FROM video LEFT JOIN user ON video.channel_id = user.id LEFT JOIN record ON video.id = record.video_id WHERE record.user_id = ?  ORDER BY record.updated DESC;', [req.id], 
     (error, result) => {
         if(error) throw error;
-        if(result.length == 0)
-            res.status(201).json({
-                'subscribe' : 'NOT Exist'
-            });
-        else{
-            res.status(200).json({
-                'result' : result
-            });
-        }
+        res.status(200).send(result);
     });
 });
 
