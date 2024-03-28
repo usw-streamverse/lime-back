ea0a1e22502304f0697941960f739b8f5d9c68c1
I4CU <129958775+IforCU@users.noreply.github.com>
2024-03-28 13:24:07 +0900

    db 파일 복구

---
+++ db/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql');
+const fs = require('fs');
+
+const config = {
+    connectionLimit: 10,
+    host: 'svlime-database.mysql.database.azure.com',
+    user: 'lime_admin',
+    password: 'Suwon18017',
+    database: 'lime_db',
+    port: 3306,
+    ssl: {ca: fs.readFileSync(`${__dirname}/DigiCertGlobalRootCA.crt.pem`)}
+}
+
+const conn = mysql.createPool(config);
+
+module.exports = conn;
